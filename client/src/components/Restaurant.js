05d3e6f2157fec5d7ab26d896cefa034a0e6967d
Naomi <nznaomizhengles@gmail.com>
2022-05-09 00:21:50 -0400

    added upload image modal

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import ImageList from "./ImageList";
-import { Link } from "react-router-dom";
+import UploadModal from './modal/UploadModal';
 
 const Restaurant = () => {
     //api call for location details based on useParams
 
     const [type, setType] = useState();
-    const [id, setID] = useState();
+    const [uploadModal, setuploadModal] = useState(false);
+    const [rid, setID] = useState(); //restaurant id
+
+    const handleCloseModals = () => {
+        setuploadModal(false)
+      };
+
+      const handleOpenUploadModal = () => {
+        setuploadModal(true);
+      };
 
     return (
         <>
-            <p></p>
+            {type && <button className='button' onClick={handleOpenUploadModal}> Upload Image</button>}
             <button onClick={() => setType("food")}> Food </button>
             <button onClick={() => setType("atmosphere")}> Atmosphere </button>
-            {type && <ImageList type={type} id={id} />}
+            {type && <ImageList type={type} rid={rid} />}
+            {uploadModal && uploadModal && (
+          <UploadModal
+            isOpen={uploadModal}
+            handleClose={handleCloseModals}
+            type = {type}
+          />
+        )}
         </>
     );
 };
