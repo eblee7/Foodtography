bb24f10d7294bec87c26eb5418f6ba322f6d20e5
eblee7 <ebleeatb@gmail.com>
2022-05-11 11:11:57 -0400

    fixed displaying images

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -10,24 +10,25 @@ const ImageList = ({ type, rid }) => {
     // user (login thing)
     // upload picture
     //uploadImage mutation
-    const [called, setCalled] = useState(false);
+    const [currentFood, setCurrentFood] = useState(null);
     const [getImages, { loading, error, data }] = useLazyQuery(
         queries.GET_RESTAURANT_IMAGES
     );
     let food = type === "food" ? true : false;
+    if (food !== currentFood) {
+        getImages({ variables: { rid, food } });
+        setCurrentFood(food);
+    }
 
     if (loading) return <p>Loading ...</p>;
     if (error) return `Error! ${error}`;
-    if (called === false) {
-        getImages({ variables: { rid, food } });
-        setCalled(true);
-    }
 
     //call getImages query
     if (data) {
         const { restaurantImages } = data;
+        console.log(restaurantImages);
         return restaurantImages.map((image) => {
-            return <img src={image.url} />;
+            return <img key={image.url} src={image.url} />;
         });
     }
 };
