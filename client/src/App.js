8a16db5b8fa173f7a085f47fb93ad365cc7bf7e9
Naomi <nznaomizhengles@gmail.com>
2022-05-13 21:24:54 -0400

    Styling with brian

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "./App.css";
-import {
-    NavLink,
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from "react-router-dom";
+import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 // import SignIn from "./components/SignIn";
 import SearchBar from "./components/SearchBar";
@@ -14,44 +9,72 @@ import Restaurant from "./components/Restaurant";
 import RestaurantList from "./components/RestaurantList";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+// import MenuIcon from '@mui/icons-material/Menu';
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new createUploadLink({
-        uri: "http://localhost:4000",
-    }),
+  cache: new InMemoryCache(),
+  link: new createUploadLink({
+    uri: "http://localhost:4000",
+  }),
 });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <header>
-                        <h1>Foodtography</h1>
-                        <nav>
-                            <NavLink to="/">Foodtography</NavLink>
-                            <NavLink to="/login">Login</NavLink>
-                        </nav>
-                        <SearchBar />
-                    </header>
-                    <br />
-                    <br />
-                    <div>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <div>
+            <header>
+              <AppBar position="static">
+                <Toolbar variant="dense">
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                  ></IconButton>
+                  <Typography variant="h4">Foodtography</Typography>
 
-                            {/* copied from lecture slides might need to change format */}
-                            {/* <Route path="/home" element={<PrivateRoute />}> */}
-                            {/* <Route path="/home" element={<Home />} /> */}
-                            {/* </Route> */}
-                            {/* <Route path="/account" element={<PrivateRoute />}> */}
-                            {/* <Route path="/account" element={<Account />} /> */}
-                            {/* </Route> */}
-                            {/* copied from lecture slides might need to change format */}
-                            {/* 
+                  <MenuItem component={Link} to="/">
+                    <Typography textAlign="center">Home</Typography>
+                  </MenuItem>
+
+                  <MenuItem component={Link} to="/login">
+                    <Typography textAlign="center">Login</Typography>
+                  </MenuItem>
+                </Toolbar>
+              </AppBar>
+            </header>
+          </div>
+          <div>
+            <SearchBar />
+          </div>
+          <br />
+          <br />
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+              {/* copied from lecture slides might need to change format */}
+              {/* <Route path="/home" element={<PrivateRoute />}> */}
+              {/* <Route path="/home" element={<Home />} /> */}
+              {/* </Route> */}
+              {/* <Route path="/account" element={<PrivateRoute />}> */}
+              {/* <Route path="/account" element={<Account />} /> */}
+              {/* </Route> */}
+              {/* copied from lecture slides might need to change format */}
+              {/* 
                         <Route path="/signin" element={<SignIn />} />
                         <Route path="/signup" element={<SignUp />} /> */}
-                            {/* <Route
+              {/* <Route
                                 path="/image/food/page/:pagenum"
                                 element={<ImageList />}
                             />
@@ -59,21 +82,18 @@ function App() {
                                 path="/image/atmosphere/page/:pagenum"
                                 element={<ImageList />}
                             /> */}
-                            <Route path="/image/:id" element={<Image />} />
-                            <Route
-                                path="/restaurants/:address"
-                                element={<RestaurantList />}
-                            />
-                            <Route
-                                path="/restaurant/:id"
-                                element={<Restaurant />}
-                            />
-                        </Routes>
-                    </div>
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+              <Route path="/image/:id" element={<Image />} />
+              <Route
+                path="/restaurants/:address"
+                element={<RestaurantList />}
+              />
+              <Route path="/restaurant/:id" element={<Restaurant />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,42 +1,48 @@
 import React from "react";
-
+import ".././App.css";
 import { useNavigate } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-    let formAddress;
-    let navigate = useNavigate();
+  let formAddress;
+  let navigate = useNavigate();
 
-    return (
-        <div>
-            <div>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        console.log(formAddress.value);
-                        navigate(`restaurants/${formAddress.value}`);
-                        formAddress.value = "";
-                    }}
-                >
-                    <div>
-                        <label>
-                            Enter Address:
-                            <br />
-                            <input
-                                required
-                                ref={(node) => {
-                                    formAddress = node;
-                                }}
-                                autoFocus={true}
-                            />
-                        </label>
-                    </div>
-
-                    <br />
-                    {<button type="submit">Search nearby address</button>}
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(formAddress.value);
+          navigate(`restaurants/${formAddress.value}`);
+          formAddress.value = "";
+        }}
+      >
+        <Paper
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Enter your location"
+            inputProps={{ "aria-label": "Enter your location" }}
+            inputRef={(node) => {
+              formAddress = node;
+            }}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
